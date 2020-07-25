bdd73fc89055d30704a7505e7d9a553d0d8c921b
ariel naniong <pablonaniong@gmail.com>
2020-07-25 17:16:03 +0800

    Updates

---
+++ precache-manifest.9ad031a0f6565a0b6d2df2b4f13b6e88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58fe2819f60d8a454e33c3f76e770a2a",
+    "url": "/coin-exchange/index.html"
+  },
+  {
+    "revision": "8e360264f10a8f104291",
+    "url": "/coin-exchange/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "612b70c1cb9d515e7de7",
+    "url": "/coin-exchange/static/js/2.a20396c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coin-exchange/static/js/2.a20396c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e360264f10a8f104291",
+    "url": "/coin-exchange/static/js/main.760af906.chunk.js"
+  },
+  {
+    "revision": "c9fe44687b1447b49608",
+    "url": "/coin-exchange/static/js/runtime-main.ea956e0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coin-exchange/static/media/logo.5d5d9eef.svg"
+  }
+]);
