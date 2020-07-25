4ed936f9c0ca54426157fb278f5e5a3a69c0da6f
ariel naniong <pablonaniong@gmail.com>
2020-07-25 15:07:46 +0800

    hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import CoinList from './components/CoinList/CoinList';
 import AccountBalance from './components/AccountBalance/AccountBalance';
 import ExchangeHeader from './components/ExchangeHeader/ExchangeHeader';
@@ -12,53 +12,18 @@ text-align: center;
 `;
 const COINS_COUNT=10;
 const formatPrice= price =>parseFloat(Number(price).toFixed(4));
-class App extends React.Component  {
-  state = {
-    showBalance:false,
-    balance: 10000,
-    coinData: [
-      // {
-      //   name:'Bitcoin' ,
-      //   ticker: 'BTC' ,
-      //   balance:'1',
-      //   price:9999.99
-      // },
-      // {
-      //   name:'Etherium' ,
-      //   ticker: 'ETH' ,
-      //   balance:'20',
-      //   price: 235.99
-      // },
-      // {
-      //   name:'Theter' ,
-      //   ticker: 'USDT' ,
-      //   balance:'4',
-      //   price:1
-      // },
-      // {
-      //   name:'Ripple' ,
-      //   ticker: 'XRP' ,
-      //   balance:'8',
-      //   price:0.2
-      // },
-      // {
-      //   name:'Bitcoin Cash' ,
-      //   ticker: 'BCH' ,
-      //   balance:'2',
-      //   price:298.99
-      // },
-      
-      
+export default  function App (props)  {
+  const [balance,setBalance]         = useState(10000);
+  const [showBalance,setShowBalance] = useState(false);
+  const [coinData,setCoinData]       = useState([]);
 
-    ]
-  }
-  
-   componentDidMount= async()=>{
-    const response =  await axios.get('https://api.coinpaprika.com/v1/coins')
+
+ const   componentDidMount= async()=>{
+    const response = await    axios.get('https://api.coinpaprika.com/v1/coins')
     const coinIds = response.data.slice(0,COINS_COUNT).map(coin=> coin.id); 
     const tickerUrl='https://api.coinpaprika.com/v1/tickers/';
     const promises=coinIds.map(id =>  axios.get(tickerUrl+id));
-    const coinData= await Promise.all(promises) 
+    const coinData=  await   Promise.all(promises) 
     const coinPriceData =coinData.map(function(response) {
       const coin= response.data;
       return{
@@ -69,42 +34,43 @@ class App extends React.Component  {
         price:formatPrice(coin.quotes.USD.price)
         };
        });
-      
-   this.setState({coinData:coinPriceData});  
+   
+    setCoinData(coinPriceData);
    }
-     
-   handleShowBalance= ()=>{
-    this.setState({showBalance:! this.state.showBalance});
-     }
+   useEffect(function(){
+    if(coinData.length===0){
+      componentDidMount();
+    } 
+   });
 
-handleRefresh=async(valueChangeId)=>{
+   const handleShowBalance= ()=>{
   
+   setShowBalance(oldValues=>!oldValues);
+     }
+   const handleRefresh=async(valueChangeId)=>{
   const response =await axios.get(`https://api.coinpaprika.com/v1/tickers/${valueChangeId}`)
+  debugger;
   const newPrice =formatPrice(response.data.quotes.USD.price); 
   
-  const newCoinData =this.state.coinData.map(function(values) {
+  const newCoinData =coinData.map(function(values) {
        let newValues={...values};
        if(values.key===valueChangeId){
           newValues.price=newPrice
         }
         return newValues;
        });
-      this.setState({coinData:newCoinData});
+     setCoinData(newCoinData);
       }; 
-
-  render(){
   return (
     <Div>
       <ExchangeHeader/>   
-      <AccountBalance amount={this.state.balance} 
-                     showBalance={this.state.showBalance }
-                   handleShowBalance={this.handleShowBalance } />
-      <CoinList   coinData={this.state.coinData} 
-                 showBalance={this.state.showBalance }
-                handleRefresh={this.handleRefresh}/>                      
+      <AccountBalance amount={balance} 
+                     showBalance={showBalance }
+                   handleShowBalance={handleShowBalance } />
+      <CoinList   coinData={coinData} 
+                 showBalance={showBalance }
+                handleRefresh={handleRefresh}/>                      
     </Div>
   );
 }
 
-}
-export default App;
